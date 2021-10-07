d7fdcfcf10b6776b58dc2747e791926e84c9f220
lethanhtuyen1008 <53989518+lethanhtuyen1008@users.noreply.github.com>
2021-10-07 12:10:40 +0700

    Codebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import Button from "@Components/materialUI/button";
+import { useRouter } from "next/router";
+import { RouteName } from "src/routers/routeName";
 
 const Home: NextPage = () => {
-    return <div className={styles.container}>index page</div>;
+    const router = useRouter();
+
+    return (
+        <div className={styles.container}>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={() => router.push(RouteName.LOGIN)}
+            >
+                Redirect
+            </Button>
+        </div>
+    );
 };
 
 export default Home;
