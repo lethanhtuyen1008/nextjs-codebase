c3130bbe03d33bffee9d33fa2ace6c021364a44a
lethanhtuyen1008 <53989518+lethanhtuyen1008@users.noreply.github.com>
2021-10-13 13:19:36 +0700

    Codebase

--- pages/api/employee/index.ts
+++ pages/api/employee/index.ts
@@ -6,7 +6,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any[]>
 ) {
-  const data = await axiosInstance.authenticatedToken(req).get("/employee");
+  const data = await axiosInstance
+    .authenticatedToken(req)
+    .get(apiEndpoints.EMPLOYEE);
 
   res.status(200).json(data.data || [{}]);
 }
